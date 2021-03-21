7d586b37f2dc02954904b6942ef2a128b8527155
Chatial's Hotel <chatialfc@gmail.com>
2021-03-22 06:46:09 +0700

    menyelesaikan vidio pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Ahmad Reyhan Ronaldo
+203040162
+https://github.com/chatial/pw2021_203040162.git
+Pertemuan 2 - 8 februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+<?php
+// Standar Ouput
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Ahmad Reyhan Ronaldo";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// ECHO $x * $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "Ahmad";
+// $nama_belakang = "Reyhan";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Ahmad";
+// $nama .= " ";
+// $nama .= "Reyhan";
+// echo $nama;
+  
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump (1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1")
+
+// Logika
+// %%, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
